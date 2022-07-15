e070ec3b1db5852e582bc712c2644dfd2e456a70
Dmytro Tashpikov <neodim84@gmail.com>
2022-07-15 18:45:26 +0300

    slider

---
+++ src/js/slider.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('.reviews').slick({
+        dots: true,
+        dotsClass: "my-dots",
+    });
+  });
+  
+  $('.single-item-rtl').slick({
+    rtl: true
+  });
