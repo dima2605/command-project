24ba9fd047dcafaabc838bcb74c5fdf4b0149cb5
dima2605 <103377412+dima2605@users.noreply.github.com>
2022-07-16 18:37:04 +0000

    Deploying to gh-pages from @ dima2605/command-project@c2244175f06da78212cc064a2942aa83701e8ee9 🚀

---
+++ index.657def64.js
@@ -0,0 +1,2 @@
+$(document).ready((function(){$(".reviews").slick({dots:!0,dotsClass:"my-dots",lazyLoad:"progressive",mobileFirst:!0,respondTo:"min"})}));
+//# sourceMappingURL=index.657def64.js.map
