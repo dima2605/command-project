c007ad3766af6af636c29b2ecb4caf263d5852eb
AllaYukhym <yukhymenko.alla@gmail.com>
2022-07-18 01:31:41 +0200

    Added no-scroll.

--- src/js/modal-products2.js
+++ src/js/modal-products2.js
@@ -3,6 +3,7 @@
     openModalBtn: document.querySelector('[data-modal-open2]'),
     closeModalBtn: document.querySelector('[data-modal-close2]'),
     modal: document.querySelector('[data-modal2]'),
+    body: document.querySelector('body'),
   };
 
   refs.openModalBtn.addEventListener('click', toggleModal);
@@ -10,5 +11,6 @@
 
   function toggleModal() {
     refs.modal.classList.toggle('is-hidden');
+    refs.body.classList.toggle('no-scroll');
   }
 })();
--- src/js/modal-products3.js
+++ src/js/modal-products3.js
@@ -3,6 +3,7 @@
     openModalBtn: document.querySelector('[data-modal-open3]'),
     closeModalBtn: document.querySelector('[data-modal-close3]'),
     modal: document.querySelector('[data-modal3]'),
+    body: document.querySelector('body'),
   };
 
   refs.openModalBtn.addEventListener('click', toggleModal);
@@ -10,5 +11,6 @@
 
   function toggleModal() {
     refs.modal.classList.toggle('is-hidden');
+    refs.body.classList.toggle('no-scroll');
   }
 })();
