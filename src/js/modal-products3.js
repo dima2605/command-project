24ff977bb94127d2ade8b1cb8dd935f4888595d4
AllaYukhym <yukhymenko.alla@gmail.com>
2022-07-18 00:36:33 +0200

    Added all modal windows.

--- src/js/modal-products3.js
+++ src/js/modal-products3.js
@@ -1,8 +1,8 @@
 (() => {
   const refs = {
-    openModalBtn: document.querySelector('[data-modal-open1]'),
-    closeModalBtn: document.querySelector('[data-modal-close1]'),
-    modal: document.querySelector('[data-modal1]'),
+    openModalBtn: document.querySelector('[data-modal-open3]'),
+    closeModalBtn: document.querySelector('[data-modal-close3]'),
+    modal: document.querySelector('[data-modal3]'),
   };
 
   refs.openModalBtn.addEventListener('click', toggleModal);
