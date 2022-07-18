81852bbbf364756f1daea1030d0d8559dee9483c
dima2605 <103377412+dima2605@users.noreply.github.com>
2022-07-18 19:06:17 +0000

    Deploying to gh-pages from @ dima2605/command-project@2b55348388c4cb999a7fbbb109406ab950d75117 🚀

---
+++ index.d9693a4a.js
@@ -0,0 +1,2 @@
+function scrollTopTop(){window.scrollTo({top:0,behavior:"smooth"})}window.addEventListener("scroll",(function(){document.querySelector(".up").classList.toggle("active",window.scrollY>500)}));
+//# sourceMappingURL=index.d9693a4a.js.map
