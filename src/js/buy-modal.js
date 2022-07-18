f871a287770d40dd13d043663df6252c44122a94
ArturAvv <avvakumovart@gmail.com>
2022-07-18 19:35:07 +0300

    Create buy-modal.js

---
+++ src/js/buy-modal.js
@@ -0,0 +1,32 @@
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector('[modal-open]'),
+    closeModalBtn: document.querySelector('[modal-close]'),
+    modal: document.querySelector('[header-modal]'),
+    body: document.querySelector('body'),
+  };
+
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle('is-hidden');
+    refs.body.classList.toggle('no-scroll');
+  }
+})();
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector('[modal-open-mobile]'),
+    closeModalBtn: document.querySelector('[modal-close-mobile]'),
+    modal: document.querySelector('[header-modal]'),
+    body: document.querySelector('body'),
+  };
+
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle('is-hidden');
+    refs.body.classList.toggle('no-scroll');
+  }
+})();
