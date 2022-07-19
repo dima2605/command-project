669aff303d55d7be898d4866838a7529777d2660
dima2605 <103377412+dima2605@users.noreply.github.com>
2022-07-19 17:37:12 +0000

    Deploying to gh-pages from @ dima2605/command-project@7b1c634be80b92718ef16d16a94856dd3627f921 🚀

---
+++ index.5eac911c.js
@@ -0,0 +1,2 @@
+!function(){var o={openModalBtn:document.querySelector("[data-modal-open1-products]"),closeModalBtn:document.querySelector("[data-modal-close1-products]"),modal:document.querySelector("[data-modal1-products]"),body:document.querySelector("body")};function e(){o.modal.classList.toggle("is-hidden"),o.body.classList.toggle("no-scroll")}o.openModalBtn.addEventListener("click",e),o.closeModalBtn.addEventListener("click",e)}();
+//# sourceMappingURL=index.5eac911c.js.map
--- index.863c35a0.js
+++
@@ -1,2 +0,0 @@
-!function(){var o={openModalBtn:document.querySelector("[data-modal-open1]"),closeModalBtn:document.querySelector("[data-modal-close1]"),modal:document.querySelector("[data-modal1]"),body:document.querySelector("body")};function e(){o.modal.classList.toggle("is-hidden"),o.body.classList.toggle("no-scroll")}o.openModalBtn.addEventListener("click",e),o.closeModalBtn.addEventListener("click",e)}();
-//# sourceMappingURL=index.863c35a0.js.map
